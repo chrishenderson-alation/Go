dce2050c413ad934c00fa3032834493a8beb2bb4
christopher-henderson <chris@chenderson.org>
2016-04-03 10:22:31 -0700

    Worker pool and echo example

---
+++ chapter1/echo.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "strings"
+    "time"
+)
+
+func cat() {
+    var s string
+    var sep string
+    for _, argument := range os.Args[1:] {
+        s += sep + argument
+        sep = " "
+    }
+    fmt.Println(s)
+}
+
+func join() {
+    fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+func main() {
+    start := time.Now()
+    cat()
+    fmt.Println(time.Now().Sub(start))
+    start = time.Now()
+    join()
+    fmt.Println(time.Now().Sub(start))
+
+    // Yay, join!
+    // 31.739µs -> string concatenation
+    // 1.164µs -> strings.Join
+}
--- chapter1/worker.go
+++ chapter1/worker.go
@@ -6,7 +6,7 @@ import (
     "time"
 )
 
-func is_prime(primes []int, candidate int) bool {
+func test(primes []int, candidate int) bool {
     square_root := math.Sqrt(float64(candidate))
     for _, prime := range primes {
         if square_root < float64(prime) {
@@ -19,8 +19,33 @@ func is_prime(primes []int, candidate int) bool {
     return true
 }
 
-func is_prime_routine(result chan bool, primes []int, candidate int) {
-    result <- is_prime(primes, candidate)
+func test_routine(primes []int, candidate int, result chan bool, quit chan bool) func() {
+    return func() {
+        square_root := math.Sqrt(float64(candidate))
+        for _, prime := range primes {
+            select {
+            case <-quit:
+                quit <- true
+                return
+            default:
+            }
+            if square_root < float64(prime) {
+                break
+            }
+            if candidate%prime == 0 {
+                result <- false
+                quit <- true
+                return
+            }
+        }
+        result <- true
+    }
+}
+
+func worker(functions chan func()) {
+    for function := range functions {
+        function()
+    }
 }
 
 func primes(ceiling int) {
@@ -28,9 +53,13 @@ func primes(ceiling int) {
     primes[0] = 2
     candidate := 3
     found := 1
+    functions := make(chan func(), 4)
+    for i := 0; i < 4; i++ {
+        go worker(functions)
+    }
     for found < ceiling {
         if found < 100 {
-            if is_prime(primes, candidate) {
+            if test(primes, candidate) {
                 primes[found] = candidate
                 found++
             }
@@ -41,10 +70,12 @@ func primes(ceiling int) {
                up goroutines is still too much.
             */
             result := make(chan bool, 4)
+            quit := make(chan bool, 4)
             bottom_chunk := 0
             top_chunk := found / 4
             for i := 0; i < 4; i++ {
-                go is_prime_routine(result, primes[bottom_chunk:top_chunk], candidate)
+                prime_slice := primes[bottom_chunk:top_chunk]
+                functions <- test_routine(prime_slice, candidate, result, quit)
                 bottom_chunk = top_chunk
                 top_chunk = top_chunk + found/4
             }
